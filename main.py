ec1935424fcb3d922c69adeb051573195e3f4cb4
George Mbugua <151632200+georgembugua00@users.noreply.github.com>
2025-06-16 13:13:32 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,53 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import requests
+import os
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
+
+# ========== PAGE CONFIG ==========
+st.set_page_config(
+    page_title="Airtel Partner Portal",
+    layout="wide",
+    initial_sidebar_state="collapsed",
+)
+
+
+# ========== PAGE SETUP ==========
+
+home_page = st.Page(
+    page="/Users/danielwanganga/Documents/Channel Partner/saidii_multi_page/sales_exec/home_page.py",
+    title="Home Page",
+    icon=":material/home:",
+    default=False
+)
+
+chat_bot = st.Page(
+    page="sales_exec/chatbot.py",
+    title=" AI Support",
+    icon=":material/support_agent:"
+)
+
+l_hub = st.Page(
+    page="sales_exec/leave.py",
+    title="Leave Management",
+    icon=":material/flight_takeoff:"
+)
+
+tickets = st.Page(
+    page="sales_exec/ticketing.py",
+    title="Raise Ticket",
+    icon=":material/sell:"
+)
+
+# ========== NAVIGATION ==========
+page_navigator = st.navigation({
+    "Home": [home_page],
+    "Help Desk": [chat_bot, tickets],
+    "Leave Hub": [l_hub]
+})
+
+page_navigator.run()
