2a8912e0faa2709a52e033561ec03a6a73186793
George Mbugua <151632200+georgembugua00@users.noreply.github.com>
2025-06-17 10:23:08 +0300

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -21,7 +21,7 @@ import httpx
 @st.cache_resource
 def init_llm_and_memory():
     llm = ChatOllama(model="llama3.2", temperature=0.4)
-    embedder = OllamaEmbeddings(model="mxbai-embed-large")
+    embedder = OllamaEmbeddings(model="nomic-embed-text")
     
     # --- New LLM session state variables ---
     if "vector_store" not in st.session_state:
